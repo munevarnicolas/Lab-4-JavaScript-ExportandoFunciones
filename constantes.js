9b3546e052f8ab8ddb3b8c7cde63190e9d75054d
Nicolas Munevar <72704297+munevarnicolas@users.noreply.github.com>
2025-11-09 23:04:08 -0500

    Add files via upload

---
+++ constantes.js
@@ -0,0 +1,16 @@
+export const PI = 3.1416;
+
+export const IVA = 0.19;
+
+export const DESCUENTO = 0.1;
+
+export const DIAS_SEMANA = 
+[
+    "Lunes", 
+    "Martes", 
+    "Miercoles", 
+    "Jueves", 
+    "Viernes", 
+    "Sabado", 
+    "Domingo"
+];
